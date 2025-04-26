2eacd4a90616fd90633204995580bdc7c2d77746
Oliver Schöttner <johnturbo82@gmail.com>
2025-04-26 09:15:32 +0200

    headlines

--- wordpress/wp-content/themes/kleber/page.php
+++ wordpress/wp-content/themes/kleber/page.php
@@ -2,6 +2,7 @@
 
 <div class="stage content">
     <?php while (have_posts()) : the_post(); ?>
+        <h1 class="title"><span><?php the_title(); ?></span></h1>
         <?php the_content(); ?>
     <?php endwhile; ?>
 </div>

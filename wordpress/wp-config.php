7188f55a25df9f8d9ca27e5bb3032cd7c74cdd45
Oliver Schöttner <johnturbo82@gmail.com>
2025-08-18 22:22:50 +0200

    adding cache

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -20,6 +20,8 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
+define('WP_CACHE', true);
+define( 'WPCACHEHOME', '/bitnami/wordpress/wp-content/plugins/wp-super-cache/' );
 define( 'DB_NAME', 'bitnami_wordpress' );
 
 /** Database username */

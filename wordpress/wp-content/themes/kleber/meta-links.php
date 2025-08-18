f80c1d27034dcbf17497055fb94dd70d847485aa
Oliver Schöttner <johnturbo82@gmail.com>
2025-08-18 22:06:22 +0200

    revert

--- wordpress/wp-content/themes/kleber/meta-links.php
+++ wordpress/wp-content/themes/kleber/meta-links.php
@@ -1,19 +1,19 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-<link rel="stylesheet" href="/wp-content/themes/kleber/css/style.css?v=<?php echo filemtime("./wp-content/themes/kleber/css/style.css"); ?>">
-<link rel="icon" type="image/svg+xml" href="/wp-content/themes/kleber/assets/images/favicon.svg">
-<link rel="icon" type="image/png" href="/wp-content/themes/kleber/assets/images/favicon.png">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css?v=<?php echo filemtime("./wp-content/themes/kleber/css/style.css"); ?>">
+<link rel="icon" type="image/svg+xml" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.svg">
+<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.png">
 <meta name="apple-mobile-web-app-status-bar-style" content="black">
 <meta name="apple-mobile-web-app-title" content="Heizungstechnik Kleber">
-<meta name="msapplication-TileImage" content="/wp-content/themes/kleber/images/icons/win8-tile-144x144.png">
+<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/images/icons/win8-tile-144x144.png">
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="msapplication-navbutton-color" content="#ffffff">
 <meta name="application-name" content="Ingolstadt Chapter" />
 <meta name="msapplication-tooltip" content="Ingolstadt Chapter" />
 <meta name="apple-mobile-web-app-title" content="Ingolstadt Chapter" />
-<meta name="msapplication-square70x70logo" content="/wp-content/themes/kleber/images/icons/win8-tile-70x70.png">
-<meta name="msapplication-square144x144logo" content="/wp-content/themes/kleber/images/icons/win8-tile-144x144.png">
-<meta name="msapplication-square150x150logo" content="/wp-content/themes/kleber/images/icons/win8-tile-150x150.png">
-<meta name="msapplication-wide310x150logo" content="/wp-content/themes/kleber/images/icons/win8-tile-310x150.png">
-<meta name="msapplication-square310x310logo" content="/wp-content/themes/kleber/images/icons/win8-tile-310x310.png">
+<meta name="msapplication-square70x70logo" content="<?php echo get_template_directory_uri(); ?>/images/icons/win8-tile-70x70.png">
+<meta name="msapplication-square144x144logo" content="<?php echo get_template_directory_uri(); ?>/images/icons/win8-tile-144x144.png">
+<meta name="msapplication-square150x150logo" content="<?php echo get_template_directory_uri(); ?>/images/icons/win8-tile-150x150.png">
+<meta name="msapplication-wide310x150logo" content="<?php echo get_template_directory_uri(); ?>/images/icons/win8-tile-310x150.png">
+<meta name="msapplication-square310x310logo" content="<?php echo get_template_directory_uri(); ?>/images/icons/win8-tile-310x310.png">
 <meta name="theme-color" content="#e4183a">

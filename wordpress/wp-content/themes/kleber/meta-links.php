50c5ee7de064fc9f990a2b9e7080ca0baff295a1
Oliver Schöttner <johnturbo82@gmail.com>
2025-08-28 10:59:43 +0200

    meta tags

--- wordpress/wp-content/themes/kleber/meta-links.php
+++ wordpress/wp-content/themes/kleber/meta-links.php
@@ -1,8 +1,17 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="<?php bloginfo('charset'); ?>">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css?v=<?php echo filemtime("./wp-content/themes/kleber/css/style.css"); ?>">
 <link rel="icon" type="image/svg+xml" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.svg">
 <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.png">
+<meta name="description" content="Heizungstechnik Kleber - Wir sind Ihr zuverlässiger Partner für Heizungen, Sanitär, Klima und Solaranlagen – sowohl im privaten als auch im gewerblichen Bereich.">
+<meta name="keywords" content="Heizung, Sanitär, Klima, Solar, Solaranlagen, Heizungstechnik, Kleber, Ingolstadt, Installation, Wartung, Reparatur">
+<meta name="robots" content="index, follow">
+<meta name="language" content="de">
+<meta name="copyright" content="Heizungstechnik Kleber">
+<meta name="revisit-after" content="7 days">
 <meta name="apple-mobile-web-app-status-bar-style" content="black">
 <meta name="apple-mobile-web-app-title" content="Heizungstechnik Kleber">
 <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/images/icons/win8-tile-144x144.png">
@@ -17,3 +26,7 @@
 <meta name="msapplication-wide310x150logo" content="<?php echo get_template_directory_uri(); ?>/images/icons/win8-tile-310x150.png">
 <meta name="msapplication-square310x310logo" content="<?php echo get_template_directory_uri(); ?>/images/icons/win8-tile-310x310.png">
 <meta name="theme-color" content="#1b1c1c">
+<meta name="developer" content="Oliver Schöttner">
+<meta name="designer" content="Oliver Schöttner">
+<link rel="designer" href="https://schoettner.dev">
+<link rel="developer" href="https://schoettner.dev">
